a157744145b51bbe0d27fa8993f4c2c331d08c22
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2021-04-16 17:16:30 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
+const LogicB = {
+draw(){
+if(!Vars.net.active() || lastAccessed == Vars.player.name) this.super$draw();
+else Draw.rect(this.region, this.x, this.y);
+}
+};
+
 LogicDisplay.buildType = () => {
   return extendContent(LogicDisplay.LogicDisplayBuild, ()
